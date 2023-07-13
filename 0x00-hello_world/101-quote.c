e0a49cbf3603c59d13ce96552484e83fc70891db
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-13 14:57:51 -0700

    fixing a problem with task no 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 *the main function, its the entry point in c
 */int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\"");
+fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
 return (1);
 }
