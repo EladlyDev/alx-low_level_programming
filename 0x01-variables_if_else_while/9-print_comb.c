57894d6a61dceab6f52335bad4ce19c63bd52972
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-16 15:45:09 -0700

    a problem in order

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,8 @@ while (i < 58) /* : after 9 in ascii */
 putchar(i);
 if (!(i == 57))
 {
-putchar(32); /* space in ascii */
 putchar(44); /* , in ascii */
+putchar(32); /* space in ascii */
 }
 i++;
 }
