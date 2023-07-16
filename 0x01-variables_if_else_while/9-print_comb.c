a0c6054070aeae4371bec9f44d1a1c2676d49f9b
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-16 15:42:40 -0700

    solving a problem with task no 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,9 @@ int i = 48; /* 0 in ascii */
 
 while (i < 58) /* : after 9 in ascii */
 {
-if (i == 57)
-{
 putchar(i);
-}
-else
+if (!(i == 57))
 {
-putchar(i);
 putchar(32); /* space in ascii */
 putchar(44); /* , in ascii */
 }
