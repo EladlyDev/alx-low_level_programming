5a307af8916595e261387b0674a2659893f89155
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-16 14:45:12 -0700

    fixing a problem with task no 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /*
  *entry point
- */init main(void)
- {
-   int i = 0;
-   while (i < 10)
-     {
-       putchar(48 + i);
-       i++;
-     }
-   putchar('\n');
-   return (0);
- }
+ */int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(48 + i);
+i++;
+}
+putchar('\n');
+return (0);
+}
