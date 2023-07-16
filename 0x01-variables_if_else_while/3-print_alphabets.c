39ec2aa9011501152ec614e3628c12f32eac1ed4
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-16 12:39:55 -0700

    fixing betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,15 @@
 *Main, Entry point
 */int main(void)
 {
-  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  int alpha_size = sizeof(alpha)/sizeof(alpha[0]) - 1;
-  int i = 0;
+char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int alpha_size = sizeof(alpha) / sizeof(alpha[0]) - 1;
+int i = 0;
 
-  while (i < alpha_size)
-    {
-      putchar(alpha[i]);
-      i++;
-    }
-  putchar('\n');
-  return(0);
+while (i < alpha_size)
+{
+putchar(alpha[i]);
+i++;
+}
+putchar('\n');
+return (0);
 }
