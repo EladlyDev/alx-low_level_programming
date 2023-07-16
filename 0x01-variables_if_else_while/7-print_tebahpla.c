72c5e00ca9f3c56ef9057096f573966dde729fd1
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-16 14:51:16 -0700

    fixing betty again

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,12 +2,12 @@
 /*
  *entry point
  */int main(void)
- {
-   int i;
-   for (i = 122; i > 96; i--)
-     {
-       putchar(i);
-     }
-   putchar('\n');
-   return (0);
- }
+{
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
