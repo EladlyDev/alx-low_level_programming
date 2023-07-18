4fc04a0328ff124425e77be9fcdfb7e57a896b19
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-18 07:03:33 -0700

    initializing files to 0x02 project and solving task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+ *Entry point
+ */int main(void)
+ {
+   puts("_putchar");
+   return (0);
+ }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file is for solving 0x02 project which is about nested loops and functions
