0289154ae64300fb8057bbf9e3bfa957d1e6129e
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-18 10:52:53 -0700

    solving task no 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * This function checks if a number greater than 0 and returns 1 and 
+ * prints +. if less than 0 returns -1 and prints -. if equals 0 retu
+ * rns 0 and prints 0
+ */int print_sign(int n)
+ {
+   if (n > 0)
+     {
+       _putchar(43);
+       return (1);
+     }
+   else if (n < 0)
+     {
+       _putchar(45);
+       return (-1);
+     }
+   else
+     {
+       _putchar(48);
+       return (0);
+
+     }
+ }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(char);
 int _isalpha(char);
+int print_sign(int);
 
 #endif
