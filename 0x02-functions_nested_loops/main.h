5f2aa66c42ec907e1d2b6abeca4689b0bbc0cecf
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-18 07:41:34 -0700

    fixing issue with the task no 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 
-void _putchar(char c)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-  putchar(c);
+	return (write(1, &c, 1));
 }
 
 void print_alphabet(void)
