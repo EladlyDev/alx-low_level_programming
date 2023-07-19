f59afde8a11d2ef312e4b26489d0d2078267ef92
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-18 17:59:47 -0700

    trying to solve task no 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,23 +3,23 @@
 /*
  * This function prints the 9 times table, starting with 0.
  */void times_table(void)
- {
-   int n, mb;
-   
-   for (mb = 0; mb < 10; mb++)
-   {
-       for (n = 0; n < 10; n++)
-       {
-           int result = mb * n;
-           
-           if (n == 0)
-           {
-              printf("%d", result);
-           }else
-           {
-               printf(",%3d", result);
-           }
-       }
-       _putchar('\n');
-   }
- }
+{
+int n, mb;
+
+for (mb = 0; mb < 10; mb++)
+{
+for (n = 0; n < 10; n++)
+{
+int sum = n * mb;
+if (n == 0)
+{
+printf("%d", sum);
+}
+else
+{
+printf(",%3d", sum);
+}
+}
+_putchar('\n');
+}
+}
