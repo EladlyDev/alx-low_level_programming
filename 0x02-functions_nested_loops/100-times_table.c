98237057361f39f0d3a000a6d2a874df02f2136c
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-18 19:27:04 -0700

    solving task no 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * This function prints the n times table, starting with 0
+ */void print_times_table(int i)
+{
+if (i < 15 && i > 0)
+{
+int n, mb;
+
+for (mb = 0; mb <= i; mb++)
+{
+for (n = 0; n <= i; n++)
+{
+int sum = n * mb;
+if (n == 0)
+printf("%d,", sum);
+
+else if (n == i)
+{
+printf("%3d\n", sum);
+}
+else
+{
+printf("%3d,", sum);
+}
+}
+}
+}
+}
