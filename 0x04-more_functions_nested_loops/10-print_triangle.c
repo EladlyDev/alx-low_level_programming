dd36e7e8f87d631bb180ae38a9557b39419d877f
Mohamed Elkholy <msrtv365@yahoo.com>
2023-07-20 15:17:32 -0700

    solving task no 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/*
+   * This function prints a triangle
+   */void print_triangle(int size)
+{
+	int i;
+
+	if (size)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			int indent = size - i, j;
+			int hash = size - indent, y;
+
+			for (j = 0; j < indent; j++)
+			{
+				_putchar(32);
+			}
+			for (y = 0; y < hash; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
