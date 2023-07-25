35a6878b5b3f768865c5ef3f14320296067f0e39
Mohamed Eladly <msrtv365@yahoo.com>
2023-07-25 15:40:08 -0700

    fixing betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,6 +3,9 @@
 /**
  * _strcpy - A function that copies a string src
  * including the '\0' to the destination dest
+ * @dest: the destination
+ * @src: the source string
+ * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
