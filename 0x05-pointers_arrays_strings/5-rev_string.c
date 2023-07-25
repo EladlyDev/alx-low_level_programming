9c6a3e96a9fb60212334d99ca65c8e84ce93a8fb
Mohamed Eladly <msrtv365@yahoo.com>
2023-07-25 14:55:10 -0700

    fixing a problem with task no 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,17 +5,20 @@
 {
 	int i = 0, count = 0;
 	int t = 0;
+	char tmp;
 	/* "My School" */
 	while (s[i] != '\0')
 	{
 		i++;
 		count++;
 	}
-	for (i = 0; count >= i; count--)
+	for (i = 0; count >= i; count--, i++)
 	{
 		if (s[count] != '\0')
 		{
+			tmp = s[t];
 			s[t] = s[count];
+			s[count] = tmp;
 			t++;
 		}
 	}
