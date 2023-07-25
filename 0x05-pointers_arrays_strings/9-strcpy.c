5c726de467665881936639711aa065986e3b5b6e
Mohamed Eladly <msrtv365@yahoo.com>
2023-07-25 15:35:02 -0700

    solving task no 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - A function that copies a string src
+ * including the '\0' to the destination dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = strlen(src);
+	int i;
+
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
