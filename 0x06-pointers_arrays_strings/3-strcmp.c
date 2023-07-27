fa12037506f6e637391eb2db2d25c9248cc8cbf5
Mohamed Eladly <msrtv365@yahoo.com>
2023-07-27 14:29:14 +0300

    solving task no 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/*
+ * _strcmp - This function compares two strings,
+ * work exactly like strcmp
+ * @s1: string no 1
+ * @s2: string no 2
+ *
+ * return: 1 if s1 > s2, -1 if s1 < s2 and 0 if they are equals
+ */int _strcmp(char *s1, char *s2)
+{
+	int i, end, result;
+	int s1_len = strlen(s1);
+	int s2_len = strlen(s2);
+
+	result = 0;
+	if (s1_len > s2_len)
+		end = s1_len;
+	else
+		end = s2_len;
+
+	for (i = 0; i < end; i++)
+	{
+		if (s1[i] > s2[i])
+			result++;
+		else if (s1[i] < s2[i])
+			result--;
+	}
+	return (result);
+}
