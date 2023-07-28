64b663a2d1be24e91ba64a6014623aa5728dade4
Mohamed Eladly <msrtv365@yahoo.com>
2023-07-28 04:10:01 +0300

    solving task no  5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <string.h>
+void range(int *, int, int);
+/*
+  * string_toupper - This function changes all lowercase to uppercase
+  * @src - the source string
+  *
+  * Return : a pointer to the result
+ **/char *string_toupper(char *src)
+{
+	int a_cap[26];
+	int a_low[26];
+	int src_len = strlen(src);
+	int i, j;
+
+	range(a_cap, 65, 90);
+	range(a_low, 97, 122);
+
+	for (i = 0; i < src_len; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (src[i] == a_low[j])
+			{
+				src[i] = a_cap[j];
+				j++;
+			}
+		}
+		i++;
+	}
+	return (src);
+}
+/*
+   */void range(int *a, int s, int e)
+{
+	int i = 0;
+
+	for (; s <= e; s++)
+	{
+		a[i] = s;
+		i++;
+	}
+}
