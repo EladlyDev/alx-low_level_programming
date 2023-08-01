c3735d402ed1db3090c0dc1da037cf893000b6dd
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-01 11:55:05 +0300

    Revert "Solving task no 6 and 7"
    This reverts commit 9a3a404a2677b3a562ab356cb6ec35e5066da4d2.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,6 @@
 /* Standard Libiraries */
 #include <stddef.h>
 #include <string.h>
-#include <stdio.h>
 
 /* function's declarations from outer files */
 int _putchar(char c);
@@ -14,7 +13,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
 
 #endif
