5a4473ff982cb40c0b2e13377db239b50c76a5b1
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-10 10:03:00 +0300

    Solving task no 0 and initialize 0x0C directory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * malloc_checked -  allocates memory using malloc.
+  * @b: unsigned int
+  *
+  * Return: a void pointer to the reserved value, 98 with exit on failure.
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptv;
+
+	ptv = malloc(b);
+	if (!ptv)
+		exit(98);
+	else
+		return(ptv);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+/* Standard Libraries */
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif
