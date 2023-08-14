9d48173a014684e21eff0c0430857408b53e62b0
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-14 09:48:55 +0300

    Solving task no 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point for the program that prints its name
+  * Return: 0 (ALWAYS) indicating succes.
+  **/
+
+int main(void)
+	{
+		printf("%s\n", __FILE__);
+
+		return (0);
+	}
