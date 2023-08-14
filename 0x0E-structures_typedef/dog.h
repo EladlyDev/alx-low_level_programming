a3e13e82ad609c94aed4d4acfb670845637b08a3
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-14 13:28:22 +0300

    Initialize 0x0E and solving task no 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+  * struct dog - the information of a dog.
+  * @name: the dog's name
+  * @age: the dog's age
+  * @owner: its owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H_ */
