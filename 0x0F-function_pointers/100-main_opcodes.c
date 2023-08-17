364fd963be35667d4f260be47ce2bf2e94c3b73e
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-17 20:21:01 +0300

    Solving task no 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]); i++)
+		printf("%.2x ", i);
+	printf("\n");
+	return (0);
+}
