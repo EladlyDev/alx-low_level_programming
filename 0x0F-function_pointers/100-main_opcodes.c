82495f80a21b54ece5f0d1ff8f3cf40258bbd9c2
Mohamed Eladly <msrtv365@yahoo.com>
2023-08-17 21:18:43 +0300

    Solving task no 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,22 +9,29 @@
  * Return: always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
+	int i, nob;
+	char *ptr = (char *) main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	nob = atoi(argv[1]);
+	if (nob < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < atoi(argv[1]); i++)
-		printf("%.2x ", i);
+	for (i = 0; i < nob; i++)
+	{
+		printf("%02x", (ptr[i] & 0xFF));
+		if (i != nob - 1)
+			printf(" ");
+	}
 	printf("\n");
+
 	return (0);
 }
